aff43a974a7c7268b88e12c6b284cf412ba0d5e5
Dustin Walker <thewalkman1234@gmail.com>
2017-05-25 15:49:22 -0500

    loops

---
+++ for-loops.js
@@ -0,0 +1,16 @@
+
+for (var i = 5; i < 120; i =i+10) {
+	console.log("By 15's", i);
+};
+
+for (var i = 4096; i > 1; i = i / 2) {
+	console.log("Division", i);
+};
+
+var presidents = ["George Washington","John Adams","Thomas Jefferson","James Madison","James Monroe"]
+
+for (var i=0; i < presidents.length; i++) {
+	console.log(presidents);
+};
+
+ 
---
+++ stacking-letters/alpha-stacker.js
@@ -0,0 +1,14 @@
+
+
+// Create an array that contains the letters of the alphabet
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+// The stackLetter function should accept the array as the sole argument
+function stackLetters (alphabet) {
+    for (var i=0; i<alphabet.length; i++) {
+    	console.log(alphabet);
+    };
+};
+
+// Invoke the function and pass in the array
+stackLetters(alphabet)
